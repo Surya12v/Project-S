85f251622f74c2796b9a145e444591a4af1e1f46
Surya-dev03 <surya.velraj@dotsito.com>
2025-07-28 11:32:56 +0530

    production

--- backend/app.js
+++ backend/app.js
@@ -43,10 +43,10 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-  httpOnly: true,
-  sameSite: 'non', // or 'none' if frontend is hosted on a different origin
-  secure: process.env.NODE_ENV === 'production',
-}
+    httpOnly: false,
+    sameSite: 'none', // or 'none' if frontend is hosted on a different origin
+    secure: process.env.NODE_ENV === 'production',
+  },
 
 }));
 
